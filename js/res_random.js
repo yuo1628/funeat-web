efd65f58cf70183eb261a3bb47986f58aa82b1f4
Eric <doshajou@gmail.com>
2013-08-01 22:41:00 +0800

    改成私有函數後成功
    Signed-off-by: Eric <doshajou@gmail.com>

--- js/res_random.js
+++ js/res_random.js
@@ -72,125 +72,6 @@ $(function() {
 })
 
 
-var randRun = function (delay, count, topMove, target ) {
-	var obj = this;
-	obj.target = target;
-	obj.currentTarget;
-	obj.delay = delay;
-	obj.count = count;
-	obj.topMove = topMove;
-	//obj.test =test;
-	
-	obj.timeRun = function() {
-		var time = window.setTimeout('new run()', obj.delay);
-	}
-	
-	
-	var i = 0;
-	var top = 0;
-	var index = 2; //第一個是假的 從1開始
-	run = function () {
-		
-		//alert(obj.test);
-		
-		i++;
-		
-		//----
-		
-		top = parseInt($(obj.target).css("top"));
-		$(obj.target).css({
-			'top' : top - obj.topMove
-		})
-		
-		if(top - 140 <= -(parseInt($(obj.target).height())))
-		{
-			$(obj.target).css({
-				'top' : -20
-			});
-		}
-		
-		//----
-		
-		if(i < obj.count) {
-			obj.timeRun();
-			
-			//index = Math.floor(top / parseInt($(".randomItem").height()));
-		}
-		else
-		{
-			i = 0;
-			//index = 0;
-		}
-		
-	};
-	
-	obj.ai = 0;
-	obj.timeStop = function (ai) {
-		obj.ai = ai;
-		
-		//alert(obj.ai);
-		
-		obj.currentTarget = $(".randomItemScrollBox[index=" + ai + "]");
-				
-		//i = obj.count;
-		
-		var t = setTimeout('stop()', obj.delay * 10);
-	}
-	
-	stop = function () {
-		
-		
-		top = parseInt($(obj.currentTarget).css("top"));
-		
-		//alert(parseInt($(obj.currentTarget).height()));
-		
-		if(top - 160 >= -(parseInt($(obj.currentTarget).height())) )
-		{
-			$(obj.currentTarget).css({
-				'top' : top - obj.topMove
-			})
-			
-			obj.timeStop(obj.ai);
-		}
-		else
-		{
-			$(obj.currentTarget).css({
-				'top' : -obj.topMove
-			})
-			
-			obj.timeHit();
-			
-			//alert("123");
-		}
-		
-		
-	}
-	
-	obj.timeHit = function () {
-		var h = setTimeout('hit()', obj.delay * 20);
-		
-		//alert("123");
-	}
-	
-	hit = function () {
-		//alert("123");
-		top = parseInt($(obj.currentTarget).css("top"));
-		if(top >= (-(index * 80)) + 30)
-		{
-			$(obj.currentTarget).css({
-				'top' : top - obj.topMove
-			})
-			
-			obj.timeHit();
-		}
-		/*
-		else
-		{
-			
-		}*/
-	}
-}
-
 /**
  * 開關事件，target為隱藏的對象，button為事件觸發者
  * 
@@ -277,25 +158,228 @@ function random() {
 		
 		$(".randomMenuBox").append(html);
 		
-		
+		/*
 		$(".randomCheckBtn").click(function() {
 			r.timeStop($(this).attr("index"));
 			$(this).unbind("click");
 		})
-		
+		*/
 	};
 	
 	
-	var r = new randRun(2,10000,4,".randomItemScrollBox");
-	$(".randomStartBtn").click(function() {
-		r.timeRun();
-			
-		$(this).unbind("click");
-	})
 	
+	$(".randomStartBtn").click(function() {
+		
+		$(this).unbind("click");
+		
+		$(".randomMenuBox").find(".randomMenu").each(function() {
+			
+			//if($(this).index()==0)
+			//{
+				var r = new randRun(2,10000,4,".randomItemScrollBox[index=" + $(this).index() + "]");
+				r.start();
+				
+				$(".randomCheckBtn[index=" + $(this).index() + "]").click(function() {
+					r.stop();
+					//$(this).unbind("click");
+				})
+			//}
+			
+		})
+	})
 	
 		
 }
 
+
+
+function randRun(delay, count, topMove, target ) {
+	var obj = this;
+	obj.target = target;
+	obj.currentTarget;
+	obj.delay = delay;
+	obj.count = count;
+	obj.topMove = topMove;
+	
+	obj.i = 0;
+	obj.index = 2;//第一個是假的 從1開始
+	
+	obj.start = function() {
+		
+		startTime();
+		
+		function startTime() {
+			var t1 = setTimeout(startRun,obj.delay);
+		}
+		
+		function startRun() {
+			obj.i++;
+			
+			var top = parseInt($(obj.target).css("top"));
+			
+			$(obj.target).css({
+				'top' : top - obj.topMove
+			})
+			
+			if(top - 140 <= -(parseInt($(obj.target).height())))
+			{
+				$(obj.target).css({
+					'top' : -20
+				});
+			}
+			
+			if(obj.i < obj.count) {
+				startTime();
+				
+			}
+			else
+			{
+				//alert(obj.i);
+				obj.i = 0;
+			}
+		}
+	}
+	
+	obj.stop = function() {
+		//alert(obj.i);
+		obj.i = obj.count;
+		//alert(obj.i);
+		
+		stopTime();
+		
+		function stopTime() {
+			var t = setTimeout(stopRun, obj.delay * 10);
+		}
+		
+		function stopRun() {
+			var top = parseInt($(obj.target).css("top"));
+		
+			if(top - 160 >= -(parseInt($(obj.target).height())) )
+			{
+				$(obj.target).css({
+					'top' : top - obj.topMove
+				})
+				
+				stopTime();
+			}
+			else
+			{
+				$(obj.target).css({
+					'top' : -obj.topMove
+				})
+				
+				stopTimeHit();
+			}
+			
+			
+		}
+		
+		function stopTimeHit() {
+			var h = setTimeout(stopHit, obj.delay * 20);
+		}
+		
+		function stopHit() {
+			var top = parseInt($(obj.target).css("top"));
+			if(top >= (-(obj.index * 80)) + 30)
+			{
+				$(obj.target).css({
+					'top' : top - obj.topMove
+				})
+				
+				stopTimeHit();
+			}
+		}
+	}
+	
+	
+	/*
+	obj.timeRun = function() {
+		var time = window.setTimeout(run, obj.delay);
+	}
+	
+	
+	function run() {
+		
+		obj.i++;
+		
+		//----
+		
+		var top = parseInt($(obj.target).css("top"));
+		$(obj.target).css({
+			'top' : top - obj.topMove
+		})
+		
+		if(top - 140 <= -(parseInt($(obj.target).height())))
+		{
+			$(obj.target).css({
+				'top' : -20
+			});
+		}
+		
+		//----
+		
+		if(i < obj.count) {
+			obj.timeRun();
+			
+			//index = Math.floor(top / parseInt($(".randomItem").height()));
+		}
+		else
+		{
+			obj.i = 0;
+			//index = 0;
+		}
+		
+	};
+	
+	//obj.ai = 0;
+	obj.timeStop = function () {
+		alert(obj.i);
+		
+		obj.i = obj.count;
+		
+		var t = setTimeout(stop, obj.delay * 10);
+	}
+	
+	function stop() {
+		var top = parseInt($(obj.target).css("top"));
+		
+		//alert(top);
+		
+		if(top - 160 >= -(parseInt($(obj.target).height())) )
+		{
+			$(obj.target).css({
+				'top' : top - obj.topMove
+			})
+			
+			obj.timeStop();
+		}
+		else
+		{
+			$(obj.target).css({
+				'top' : -obj.topMove
+			})
+			
+			obj.timeHit();
+		}
+	}
+	
+	obj.timeHit = function () {
+		var h = setTimeout(hit, obj.delay * 20);
+		
+	}
+	
+	function hit() {
+		var top = parseInt($(obj.target).css("top"));
+		if(top >= (-(obj.index * 80)) + 30)
+		{
+			$(obj.target).css({
+				'top' : top - obj.topMove
+			})
+			
+			obj.timeHit();
+		}
+	}
+	*/
+}
 
 
